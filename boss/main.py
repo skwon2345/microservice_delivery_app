3e621b65f7e7420c110f3b828f4e482f9be656bf
sukkwono-hits <sukkwono@hits.ai>
2021-12-17 18:42:46 +0900

    boss: main: apply ORM in Flask with SQLAlchemy
    - some api is created

--- boss/main.py
+++ boss/main.py
@@ -1,6 +1,11 @@
-from flask import Flask
+from dataclasses import dataclass
+
+import requests
+from flask import Flask, abort, jsonify
 from flask_cors import CORS
 from flask_sqlalchemy import SQLAlchemy
+
+from producer import publish
 
 app = Flask(__name__)
 app.config["SQLALCHEMY_DATABASE_URI"] = "mysql://root:root@db/main"
@@ -11,21 +16,38 @@ CORS(app)
 db = SQLAlchemy(app)
 
 
-class Shop:
+@dataclass
+class Shop(db.Model):
+    id: int
+    shop_name: str
+    shop_address: str
+
     id = db.Column(db.Integer, primary_key=True, autoincrement=False)
     shop_name = db.Column(db.String(200))
     shop_address = db.Column(db.String(200))
 
 
-class Order:
+@dataclass
+class Order(db.Model):
+    id: int
+    shop: int
+    address: str
+
     id = db.Column(db.Integer, primary_key=True, autoincrement=False)
     shop = db.Column(db.Integer)
     address = db.Column(db.String(200))
 
 
-@app.route("/")
+@app.route("/api/shop")
 def index():
-    return "hello"
+    return jsonify(Shop.query.all())
+
+
+@app.route("/api/order/<int:id>/deliver_finish", methods=["POST"])
+def deliver_finish(id):
+    publish("order_deliver_finished", id)
+
+    return jsonify({"message": "success"})
 
 
 if __name__ == "__main__":
